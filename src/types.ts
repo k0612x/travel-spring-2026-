1c7f4877851ceb6e798eb5b3e234c5ebd1e06bb8
Kangxun <122718931+k0612x@users.noreply.github.com>
2026-01-22 16:35:18 +0800

    Add Destination interface to types.ts

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface Destination {
+  id: number
+  name: string
+  country: string
+  description: string
+  image: string
+  price: number
+  duration: string
+  rating: number
+  highlights: string[]
+}
